e7eb501c77a138e68127ce15f8b2cd325db36359
nishanta sapkota <nishantasapkota@gmail.com>
2024-01-09 10:16:27 +0545

    update

--- src/components/content.js
+++ src/components/content.js
@@ -3,8 +3,10 @@
 import React from "react";
 
 import Accordian from "./accordian";
+import useMediaQuery from "@/hooks/useMediaQuery";
 
 const Content = ({ title, body }) => {
+  const tablet = useMediaQuery("(max-width: 1024px)");
   return (
     <div className="text-left gap-11  py-12 px-4 sm:px-6 lg:px-8 grid lg:grid-cols-2  bg-yellow-500 rounded-3xl">
       <div className="">
@@ -15,12 +17,19 @@ const Content = ({ title, body }) => {
           beatae? Lorem ipsum dolor sit amet consectetur adipisicing elit.
           Error, beatae?
         </p>
-        <button className="border-2 rounded-full border-black border-solid mt-8 py-5 px-11 text-lg font-semibold text-white uppercase transition-colors duration-200 transform bg-red-600 rounded-lg shadow-xl hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-75">
-          Discover Product
-        </button>
+        {!tablet && (
+          <button className="border-2 rounded-full border-black border-solid mt-8 py-5 px-11 text-lg font-semibold text-white uppercase transition-colors duration-200 transform bg-red-600 rounded-lg shadow-xl hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-75">
+            Discover Product
+          </button>
+        )}
       </div>
       <div>
         <Accordian />
+        {tablet && (
+          <button className="border-2 rounded-full border-black border-solid mt-8 py-5 px-11 text-lg font-semibold text-white uppercase transition-colors duration-200 transform bg-red-600 rounded-lg shadow-xl hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-75">
+            Discover Product
+          </button>
+        )}
       </div>
     </div>
   );
